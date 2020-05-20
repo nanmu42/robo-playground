fbb11ce417be7f83f0e91aff26089299df5147b6
nanmu42 <i@nanmu.me>
2020-05-20 18:20:29 +0800

    goalkeeper: partial implement

--- goalkeeper.py
+++ goalkeeper.py
@@ -2,16 +2,24 @@ import enum
 import logging
 import multiprocessing as mp
 import pickle
+from typing import Tuple, List, Optional
 
 import click
 import cv2 as cv
 import simple_pid
 
+import measure
 import robomaster as rm
 from robomaster import CTX
 
 rm.LOG_LEVEL = logging.DEBUG
 pickle.DEFAULT_PROTOCOL = pickle.HIGHEST_PROTOCOL
+
+GREEN_LOWER = (29, 90, 90)
+GREEN_UPPER = (64, 255, 255)
+BALL_ACTUAL_RADIUS = 0.065 / 2
+
+QUEUE_TIMEOUT: float = 0.5
 
 
 @enum.unique
@@ -45,9 +53,10 @@ class KeeperMind(rm.Worker):
         self._vision = vision
         self._push = push
         self._event = event
-        self._logger = self.get_logger()
         self._pid: simple_pid.PID = simple_pid.PID(*self.PID_PARAMS, setpoint=0, sample_time=1 / 30, output_limits=(0, 2.5))
         self._position: rm.ChassisPosition = rm.ChassisPosition(0, 0, 0)
+        self._ball_distances: Optional[Tuple[float, float]] = None
+        self._ball_last_seen: Optional[float] = None
 
         self._cmd = rm.Commander(ip, timeout)
         self._cmd.gimbal_moveto(pitch=-10)
@@ -64,6 +73,7 @@ class KeeperMind(rm.Worker):
 
     def _init_state(self):
         if self._state == KeeperState.WATCHING:
+            # TODO: 回中，回正
             self._cmd.led_control(rm.LED_ALL, rm.LED_EFFECT_PULSE, 0, 255, 0)
         elif self._state == KeeperState.CHASING:
             self._pid.reset()
@@ -82,11 +92,11 @@ class KeeperMind(rm.Worker):
     def _kick(self):
         pass
 
-    def _update(self):
-        pass
+    def _tick(self):
+        forward, lateral = self._vision.get(block=True, timeout=QUEUE_TIMEOUT)
 
     def work(self) -> None:
-        self._update()
+        self._tick()
 
         if self._state == KeeperState.WATCHING:
             self._watch()
@@ -98,23 +108,72 @@ class KeeperMind(rm.Worker):
             raise ValueError(f'unknown state {self._state}')
 
 
-def vision(frame, **kwargs) -> None:
-    cv.imshow("frame", frame)
+def contour_analysis(cnt) -> Tuple[int, int]:
+    approx = cv.approxPolyDP(cnt, 0.01 * cv.arcLength(cnt, True), True)
+    area = cv.contourArea(cnt)
+    return len(approx), area
+
+
+def biggest_circle_cnt(cnts: List):
+    found_cnt = None
+    found_edges = 0
+    found_area = 0
+
+    for cnt in cnts:
+        edges, area = contour_analysis(cnt)
+        if edges > 8 \
+                and 260 < area < 20000 \
+                and edges > found_edges \
+                and area > found_area:
+            found_edges = edges
+            found_area = area
+            found_cnt = cnt
+
+    return found_cnt
+
+
+def vision(frame, logger: logging.Logger) -> Optional[Tuple[float, float]]:
+    processed = cv.GaussianBlur(frame, (11, 11), 0)
+    processed = cv.cvtColor(processed, cv.COLOR_BGR2HSV)
+
+    mask = cv.inRange(processed, GREEN_LOWER, GREEN_UPPER)
+    mask = cv.morphologyEx(mask, cv.MORPH_OPEN, None)
+    cnts, _ = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+    ball_cnt = biggest_circle_cnt(cnts)
+    if ball_cnt is None:
+        cv.putText(frame, 'no ball detected', (50, 20), cv.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
+        cv.imshow('vision', frame)
+        cv.waitKey(1)
+        return None
+
+    (x, y), pixel_radius = cv.minEnclosingCircle(ball_cnt)
+    distance = measure.pinhole_distance(BALL_ACTUAL_RADIUS, pixel_radius)
+    forward, lateral = measure.distance_decomposition(x, distance)
+    cv.circle(frame, (x, y), pixel_radius, (0, 255, 0), 2)
+    cv.circle(frame, (x, y), 1, (0, 0, 255), 2)
+    cv.putText(frame, 'forward: %.1f cm' % forward * 100, (50, 20), cv.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
+    cv.putText(frame, 'lateral: %.1f cm' % lateral * 100, (50, 70), cv.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
+
+    cv.imshow('vision', frame)
     cv.waitKey(1)
+
+    return forward, lateral
 
 
 @click.group()
 @click.option('--ip', default='', type=str, help='(Optional) IP of Robomaster EP')
 @click.option('--timeout', default=10.0, type=float, help='(Optional) Timeout for commands')
-@click.option('--width', default=1.0, type=float, help='(Optional) Field width')
-@click.option('--depth', default=1.0, type=float, help='(Optional) Field depth')
-def cli(ctx: click.Context, ip: str, timeout: float, width: float, depth: float):
+@click.option('--max-width', default=1.0, type=float, help='(Optional) Field width')
+@click.option('--max-depth', default=1.0, type=float, help='(Optional) Field depth')
+@click.pass_context
+def cli(ctx: click.Context, ip: str, timeout: float, max_width: float, max_depth: float):
     ctx.ensure_object(dict)
     ctx.obj['manager']: mp.managers.SyncManager = CTX.Manager()
     ctx.obj['ip']: str = ip
     ctx.obj['timeout']: float = timeout
-    ctx.obj['width']: float = width
-    ctx.obj['depth']: float = depth
+    ctx.obj['max_width']: float = max_width
+    ctx.obj['max_depth']: float = max_depth
 
 
 @cli.command()
